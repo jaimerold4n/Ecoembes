2d3dc7974094774eac85dc45995c3a505a0536ec
Jaime Roldán <160106862+jaimerold4n@users.noreply.github.com>
2025-11-06 12:45:16 +0100

    Initial commit

---
+++ src/main/java/com/ecoembes/ecoembes/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.ecoembes.ecoembes.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody String body) {
+        return ResponseEntity.ok("Login correcto ✅");
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout() {
+        return ResponseEntity.noContent().build();
+    }
+}
