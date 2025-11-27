c189475efedac82118c27172da455a276a0ec65e
Oier Artabe <oier.artabe@opendeusto.es>
2025-11-27 13:20:43 +0100

    Creacion nuevo DTO

---
+++ src/main/java/com/ecoembes/dto/CapacidadPlantaRemotaDTO.java
@@ -0,0 +1,22 @@
+package com.ecoembes.dto;
+
+public class CapacidadPlantaRemotaDTO {
+    private String id;
+    private Double capacidad;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Double getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(Double capacidad) {
+        this.capacidad = capacidad;
+    }
+}
