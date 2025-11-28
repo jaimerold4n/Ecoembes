ce7ed14503c70d3fc022f5e0e7f30c66544bf5f1
aroa-im <aroa.i.m@opendeusto.es>
2025-11-28 19:59:16 +0100

    crear servidorPlasSb

---
+++ ServidorPlasSB/src/main/java/com/ecoembes/plasb/AplicacionPlasSb.java
@@ -0,0 +1,13 @@
+package com.ecoembes.plasb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionPlasSb {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplicacionPlasSb.class, args);
+    }
+
+}
