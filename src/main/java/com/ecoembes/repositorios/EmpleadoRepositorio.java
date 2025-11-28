aa706fbfb74b7c0256621de9eb2df5593f3f38a8
aroa-im <aroa.i.m@opendeusto.es>
2025-11-28 20:02:41 +0100

    .

--- src/main/java/com/ecoembes/repositorios/EmpleadoRepositorio.java
+++ src/main/java/com/ecoembes/repositorios/EmpleadoRepositorio.java
@@ -1,3 +1,5 @@
+package com.ecoembes.repositorios;
+
 import com.ecoembes.domain.Empleado;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
