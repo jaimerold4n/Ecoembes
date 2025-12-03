dd02efbcad88936a649335a84da182c47593b884
Oier Artabe <oier.artabe@opendeusto.es>
2025-12-03 12:59:57 +0100

    Cambios de nombre a archivos de carpeta remoto

---
+++ Ecoembes/src/main/java/com/ecoembes/service/remoto/FabricaSockets.java
@@ -0,0 +1,8 @@
+package com.ecoembes.service.remoto;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public interface FabricaSockets {
+    Socket crearSockets(String anfitrion, int puerto) throws IOException;
+}
